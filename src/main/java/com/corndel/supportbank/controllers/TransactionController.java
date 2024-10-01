3be6b5e1338c42bb575316c95e2cca66d27066bb
Damian <damiangildea12@gmail.com>
2024-10-01 15:59:17 +0100

    Refactored list and prints

--- src/main/java/com/corndel/supportbank/controllers/TransactionController.java
+++ src/main/java/com/corndel/supportbank/controllers/TransactionController.java
@@ -1,7 +1,8 @@
 package com.corndel.supportbank.controllers;
+import com.corndel.supportbank.services.TransactionListService;
 import com.corndel.supportbank.services.TransactionService;
 import picocli.CommandLine.Command;
 
-@Command(name = "transaction", subcommands = {TransactionService.class})
+@Command(name = "transaction", subcommands = {TransactionService.class, TransactionListService.class})
 public class TransactionController {
 }
